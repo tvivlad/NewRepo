a7ae4ab2f44a9a75630698c1812348dd7091e2b1
tvivlad <35404251+tvivlad@users.noreply.github.com>
2020-05-15 00:31:13 +0500

    Новое обновление 2

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -27,7 +27,7 @@ namespace WindowsFormsApp1
             textBox1.Text = "обновление текста";
             Form1.ActiveForm.Text = "Привет мир!!!";
             textBox1.Text = "новое обновление!!!";
-
+            textBox1.Text = "Новое обновление 2!!!";
         }
     }
 }
